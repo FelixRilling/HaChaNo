c28a92d12298a9771bcac7b2b37f2352e235bc7d
FelixRilling <contact@rilling.dev>
2021-12-25 17:28:31 +0100

    Close config file after use.

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ logging.basicConfig(level=logging.INFO)
 with open(r"./config.yaml", encoding="utf8") as file:
 	config = yaml.load(file, Loader=yaml.SafeLoader)
 
-	sounds: Dict[str, str] = config.get("sounds")
-	if sounds is None:
-		raise ValueError("'sounds' was not found in config.")
+sounds: Dict[str, str] = config.get("sounds")
+if sounds is None:
+	raise ValueError("'sounds' was not found in config.")
 
-	HaChaNo(sounds).start()
+HaChaNo(sounds).start()
